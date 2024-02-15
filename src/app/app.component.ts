48271e41e5517f755140be2c22f06bb83e7c6a9f
Luis <ariza.9485@gmail.com>
2024-02-15 15:13:57 -0500

    terminado de ajustar component social- media  ajustes de estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './src/components/toolbar/toolbar.component';
 import { WelcomeComponent } from './src/components/welcome/welcome.component';
+import { SocialMediaComponent } from './src/components/social-media/social-media.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ ToolbarComponent, WelcomeComponent, RouterOutlet ],
+  imports: [ ToolbarComponent, WelcomeComponent, RouterOutlet, SocialMediaComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
